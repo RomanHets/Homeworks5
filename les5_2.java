94e65af6dd7caa4f47ac83d8f08cd13f8b200450
RomanHets <gromi4707@gmail.com>
2022-08-19 14:24:25 +0300

    HomeWork5_2

---
+++ les5_2.java
@@ -0,0 +1,9 @@
+public class les5_2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            if (i % 14 == 0)
+                System.out.println(i);
+        }
+    }
+}
+
